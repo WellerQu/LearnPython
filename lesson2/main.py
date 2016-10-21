7a9ed5945a2619cda6e9d5d0949d1a14f82cd603
nixon <xiaoyao.ning@gmail.com>
2016-10-21 11:45:22 +0800

    添加lesson2

---
+++ lesson2/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# this is a comment
+
+a = int(raw_input('input number:'))
+
+if a > 100:
+    print 'More than 100'
+elif a == 100:
+    print 'equals 100'
+else:
+    print 'Less than 100'
+
+if True:
+    c = 21
+
+print c
