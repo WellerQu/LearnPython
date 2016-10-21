9124be20ff433648b81b6509c2818d387a7f1d78
nixon <xiaoyao.ning@gmail.com>
2016-10-21 14:35:07 +0800

    完成lesson2学习

--- lesson2/main.py
+++ lesson2/main.py
@@ -14,3 +14,31 @@ if True:
     c = 21
 
 print c
+
+b = 12
+c = 5
+print b / c
+
+b = 12.0
+c = 5
+print b / c
+
+print '''multi line
+Hello
+World'''
+
+if None:
+    print 'None is a True'
+else:
+    print 'None is a False'
+
+if None == False:
+    print 'None equals to False'
+
+if 0:
+    print '0 is a True'
+else:
+    print '0 is a False'
+
+if 0 == False:
+    print '0 equals to False'
