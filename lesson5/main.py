d120f9424ca8eabfcd647345e78c3fb93b83a832
nixon <xiaoyao.ning@gmail.com>
2016-11-08 22:14:29 +0800

    添加知识点

--- lesson5/main.py
+++ lesson5/main.py
@@ -19,3 +19,15 @@ def cSum(a, b):
 
 L = reduce(cSum, [1, 2, 3])
 print L
+
+
+def primeNum(n):
+    if n == 2 or n == 3:
+        return True
+    elif n > 3 and n % 2 != 0 and n % 3 != 0:
+        return True
+    else:
+        return False
+
+L = filter(primeNum, range(1, 101))
+print L
