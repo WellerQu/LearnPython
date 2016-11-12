8fa7634d5f181407d3aad860081f244875bf5636
nixon <xiaoyao.ning@gmail.com>
2016-11-12 15:32:02 +0800

    添加知识点

---
+++ lesson6/web/a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+这是一个神奇的模块
+'''
+
+__author__ = "nixon"
+
+
+def _p():
+    print 'World'
+
+
+def sayHello():
+    print 'Hello'
+    _p()
+
+print __name__
